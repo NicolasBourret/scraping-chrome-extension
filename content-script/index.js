d11c210ece2f23b772915b29c89049a454b9e365
Nicolas Bourret <nicolas.bourret48@gmail.com>
2023-09-18 21:38:59 +0200

    create content-script file

---
+++ content-script/index.js
@@ -0,0 +1,20 @@
+const elementsOvered = [];
+
+const selectElement = (element) => {
+  element.addEventListener("click", () => {
+    element.style.border = "none";
+    document.removeEventListener("mouseover", addAndRemoveBorder);
+  });
+};
+
+const addAndRemoveBorder = (e) => {
+  if (elementsOvered.length > 0) {
+    elementsOvered[0].style.border = "none";
+    elementsOvered.shift();
+  }
+  const element = e.target;
+  element.style.border = "1px solid green";
+  elementsOvered.push(element);
+
+  selectElement(element);
+};
