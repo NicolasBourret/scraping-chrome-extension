4a09f72aae7c656734a1280d3b06949e7dfce716
Nicolas Bourret <nicolas.bourret48@gmail.com>
2023-10-03 21:36:05 +0200

    fix bug about elementWithListener undefined

--- content_script/eventHandlers/index.js
+++ content_script/eventHandlers/index.js
@@ -1,25 +1,17 @@
-const addElementBorder = (event) => {
+export const removeElement = (elementToRemove) => {
+  document.body.removeChild(elementToRemove);
+};
+
+export const addElementBorder = (event) => {
   const element = event.target;
   element.style.outline = "2px solid black";
 };
 
-const removeElementBorder = (event) => {
+export const removeElementBorder = (event) => {
   const element = event.target;
   element.style.outline = "none";
 };
 
-export const handleElementBorder = ({ target }) => {
-  const element = target;
-
-  element.addEventListener("mouseover", addElementBorder);
-  element.addEventListener("mouseout", removeElementBorder);
-  elementWithListener.push(element);
-};
-
 export const removeEvent = (element) => {
   element.removeEventListener("mouseover", addElementBorder);
-};
-
-export const removeElement = (elementToRemove) => {
-  document.body.removeChild(elementToRemove);
 };
