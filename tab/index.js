ea296083c91d17461e17acc2986644a3586830e1
Nicolas Bourret <nicolas.bourret48@gmail.com>
2023-11-04 15:06:54 +0100

    cleanup

--- tab/index.js
+++ tab/index.js
@@ -1,7 +1,4 @@
-console.log("ok");
-
 chrome.runtime.onMessage.addListener((req, sender, sendResponse) => {
-  console.log("test");
   if (req.data) {
     console.log(req.data);
     sendResponse({ message: "data are succesfully sent" });
